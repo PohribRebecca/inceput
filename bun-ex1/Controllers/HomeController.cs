446ca69bfa5a77fbe28dab6e4b13a3e238704c14
Pohrib Rebecca <pohribrebecca1@gmail.com>
2021-11-26 17:30:05 +0200

    Add project files.

---
+++ bun-ex1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bun_ex1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
